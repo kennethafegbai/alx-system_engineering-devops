626c58d8b2abf4c7d65fd80016ec108fcadd4577
Kenneth Afegbai <kennethafegbai@gmail.com>
2023-04-04 09:33:33 +0100

    fix error in puppet file installing flask 2.1.0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,7 +2,13 @@ class { 'python':
 	ensure => 'installed',
 }
 
-python::pip { 'flask':
-	ensure => '2.1.0',
+package { 'python3-pip':
+	ensure => 'installed',
 	require => Class['python'],
+}
+
+exec { 'install-flask':
+	command => '/usr/bin/pip3 install Flask==2.1.0',
+	creates => '/usr/local/lib/python3.8/dist-packages/flask',
+	require => Package['python3-pip'],
 }
