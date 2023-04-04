69180b886b57319b2101312ce464af2064f9da9f
Kenneth Afegbai <kennethafegbai@gmail.com>
2023-04-04 09:49:50 +0100

    change flask version to 2.1.0 using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,6 @@
-class { 'python':
-	ensure => 'installed',
-}
+# install flask from pip3 using Puppet
 
-package { 'python3-pip':
-	ensure => 'installed',
-	require => Class['python'],
-}
-
-exec { 'install-flask':
-	command => '/usr/bin/pip3 install Flask==2.1.0',
-	creates => '/usr/local/lib/python3.8/dist-packages/flask',
-	require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
